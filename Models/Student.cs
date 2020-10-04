064c9fd7d853bda51d1f41128a5197b1da62f25d
Grant Hadley <gh900416@ohio.edu>
2020-10-04 16:57:57 -0400

    d

--- Models/Student.cs
+++ Models/Student.cs
@@ -10,9 +10,13 @@ namespace gh900416_MIS4200.Models
     public class Student
     {
         public int studentID { get; set; }
-
+        [Display (Name ="Last Name")]
+        [Required(ErrorMessage ="Last Name Required")]
+        [StringLength(20)]
         public string lastName { get; set; }
-
+        [Display(Name = "First Name")]
+        [Required(ErrorMessage = "First Name Required")]
+        [StringLength(20)]
         public string firstName { get; set; }
         public ICollection<Class> classes { get; set; }
     }
