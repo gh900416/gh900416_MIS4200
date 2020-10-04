84e8f50a3b7b405c9fbeef2b1489940c02b9cdfe
Grant Hadley <gh900416@ohio.edu>
2020-10-04 16:43:47 -0400

    SD3

--- Models/Class.cs
+++ Models/Class.cs
@@ -8,11 +8,8 @@ namespace gh900416_MIS4200.Models
 {
     public class Class
     {
-        [Required(ErrorMessage ="ClassID required")]
         public int classID { get; set; }
         [Display (Name ="Course Name")]
-        [Required(ErrorMessage ="Course Name required")]
-        [StringLength(20)]
         public string title { get; set; }
         [Display (Name ="Description")]
         public string description { get; set; }
